22dd0fe2a122f7a53bbdc1c7731631441e89f9fe
Daniel Cavalcante <danielcdesouza@gmail.com>
2019-03-30 16:21:45 -0300

    Change port

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.get('', function (req, res) {
 
 require('./src/pet.routes.js')(app);
   
-app.listen(3000, () => {
-  console.log('Server is listening on port: 3000');
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log('Server is listening on port: ' + port);
 });
